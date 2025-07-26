0255fe94f762cdaad0c95d7d2a1096d6bb5e9949
Jhm2003 <jorgeenrique.hm@gmail.com>
2025-07-26 10:10:49 -0600

    cambios

--- desafio1/src/app/page.js
+++ desafio1/src/app/page.js
@@ -1,95 +1,57 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
-
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+ const [numero1, setNumero1] = useState('');
+ const [numero2, setNumero2] = useState('');
+ const [resultado, setResultado] = useState(null);
+ const sumar = () => {
+ const resultadoSuma = parseFloat(numero1) + parseFloat(numero2);
+ setResultado(`Resultado de la suma: ${resultadoSuma}`);
+ };
+ const restar = () => {
+ const resultadoResta = parseFloat(numero1) - parseFloat(numero2);
+ setResultado(`Resultado de la resta: ${resultadoResta}`);
+ };
+ const multi = () => {
+ const resultadoMulti = parseFloat(numero1) * parseFloat(numero2);
+ setResultado(`Resultado de la resta: ${resultadoMulti}`);
+ };
+ const raiz = () => {
+ const resultadoRaiz = Math.sqrt(parseFloat(numero1)) ;
+ setResultado(`Resultado de la resta: ${resultadoRaiz}`);
+ };
+ const dividir = () => {
+ const resultadoDividir = parseFloat(numero1) / parseFloat(numero2);
+ setResultado(`Resultado de la resta: ${resultadoDividir}`);
+ };
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+ return (
+ <main className={styles.main}>
+ <div className={styles.calculadora}>
+ <div className={styles.numeros}>
+ <label className={styles.text}>Número 1:</label>
+ <input className={styles.inputnum} type="number"
+value={numero1} onChange={(e) => setNumero1(e.target.value)} />
+ </div>
+ <div className={styles.numeros}>
+ <label className={styles.text} >Número 2:</label>
+ <input className={styles.inputnum} type="number"
+value={numero2} onChange={(e) => setNumero2(e.target.value)} />
+ </div>
+ <div>
+ <button className={styles.button}
+onClick={sumar}>Sumar</button>
+ <button className={styles.button}
+onClick={restar}>Restar</button>
+ </div>
+ <button className={styles.button}
+onClick={multi}>Multiplicar</button>
+ 
+ {resultado && <div
+className={styles.resultado}>{resultado}</div>}
+ </div>
+ </main>
+ );
 }
+
